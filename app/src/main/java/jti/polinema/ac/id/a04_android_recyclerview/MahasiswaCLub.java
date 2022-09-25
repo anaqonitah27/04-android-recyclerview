548e6a0f70906d5055a4208713ed943e5691607c
Ana Qonitah Munawwaroh <75164626+anaqonitah27@users.noreply.github.com>
2022-09-25 22:54:02 +0700

    Tugas RecyclerView

---
+++ app/src/main/java/jti/polinema/ac/id/a04_android_recyclerview/MahasiswaCLub.java
@@ -0,0 +1,6 @@
+package jti.polinema.ac.id.a04_android_recyclerview;
+
+public class MahasiswaCLub {
+    private String nama;
+
+}
---
+++ app/src/main/java/jti/polinema/ac/id/a04_android_recyclerview/MainActivity.java
@@ -0,0 +1,14 @@
+package jti.polinema.ac.id.a04_android_recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
